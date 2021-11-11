50bae8433ebbc2b35c327637b862864a47b3afbd
jose-oshin <oshindeveloper15@gmail.com>
2021-11-11 11:02:06 -0400

    created color picker

---
+++ lib/core/widgets/color_picker/color_picker.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+///Displays a color picker from an array of [Color]
+///
+///note: please don't repeat colors in the color list, this may cause UI inconsistences :D
+class ColorPicker extends StatefulWidget{
+
+  final List<Color> colors;
+  final Function(Color) onColorTap;
+  final Color? initialSelectedcolor;
+
+  ColorPicker({
+    required this.colors,
+    required this.onColorTap,
+    this.initialSelectedcolor,
+  });
+  
+  @override
+  _ColorPickerState createState()=> _ColorPickerState();
+
+}
+
+
+class _ColorPickerState extends State<ColorPicker>{
+
+  late Color _currentSelectedColor;
+
+  @override
+  void initState() {
+    super.initState();
+      _currentSelectedColor = widget.initialSelectedcolor??widget.colors[0];
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return  Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount:4,
+          mainAxisSpacing: 3,
+          crossAxisSpacing: 3,
+          children: colorsToTiles()
+          )
+      )
+    );
+  }
+
+  List<_ColorTile> colorsToTiles(){
+
+    return widget.colors.map((color){
+      
+      return _ColorTile(
+        onTileTap: (currentColor){
+          
+          setState(() {
+            _currentSelectedColor = color;  
+          });
+
+          widget.onColorTap(currentColor);
+        },
+        isSelected:_currentSelectedColor.value == color.value,
+        tileColor: color
+      );
+    }).toList();
+
+  }
+
+}
+
+class _ColorTile extends StatelessWidget{
+  final Color tileColor;
+  final Function(Color) onTileTap;
+  final bool isSelected;
+  
+
+   _ColorTile({
+    required this.onTileTap,
+    required this.tileColor,
+    required this.isSelected
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: (){
+
+        onTileTap(tileColor);
+      },
+      child: Stack(
+        fit: StackFit.expand,
+        alignment: Alignment.center,
+        children:[
+
+          Positioned(
+            child: Container(
+              width: 47,
+              height: 47,
+              color: tileColor ,
+            ),
+          ),
+
+          Positioned(
+            width: 50,
+            height: 50,
+            child: isSelected?
+
+             Container(
+              width: 10,
+              height: 10,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: const Color.fromRGBO(0, 0, 0, 0.4)
+              ),
+              child: const Icon(
+                Icons.check,
+                color:Colors.white
+                ),
+            )
+
+            :
+
+            Container()
+          )
+          
+        ] 
+      ),
+    );
+  }
+
+}
