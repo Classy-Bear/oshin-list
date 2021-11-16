5ce4254e6b9f3f2ebb11bac674009e5c16e00cf5
karen-oshin <83609740+karen-oshin@users.noreply.github.com>
2021-11-16 19:27:15 -0400

    task information (#31)
    * Update global.dart
    
    * Delete task_repository_test.dart
    
    * agregando el metodo dispose
    
    * test de fechar los task, hecho
    
    * creating update and get on task
    
    * eliminando metodo get cual no es necesario
    
    * eliminando constructor publico
    
    * eliminando metodo get
    
    * agregando propiedad completed
    
    * creando la libreria de task_repository
    
    * configurando el linter
    
    * creando task helper y form task
    
    * cambiando a hexadecimal los colores del helper
    
    * agregando filtros a getALl
    
    * fixing task helper close #24
    
    * implementando selected color close #21
    
    * agregando metodo que retorna el type como string y el date en el formato dd/mm/yyyy
    
    * implementando date
    
    * formateo
    
    * refactorizando las extensiones de Task y creando la extension del enum Type
    
    * eliminando intl y creando extension de DateTime formaterDate
    
    * moviendo la extension de dart en su propio archivo
    
    * creando task type helper
    
    * type to enum
    
    * eliminando task helper
    
    * validators
    
    * fixing error syntax
    
    * implementando picker color
    
    * llamando formulario
    
    * creando task information
    
    Co-authored-by: david-oshin <83610008+david-oshin@users.noreply.github.com>
    Co-authored-by: David Acevedo <37255403+Classy-Bear@users.noreply.github.com>

--- lib/modules/task_information/view/task_information_page.dart
+++ lib/modules/task_information/view/task_information_page.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:oshin_list/core/widgets/form_task/form.dart';
 
+class TaskInformationPage extends StatelessWidget {
+  static const route = '/taskInformationPage';
+
+  const TaskInformationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const TaskInformationView();
+  }
+}
+
+class TaskInformationView extends StatelessWidget {
+  const TaskInformationView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Oshin Tasklist",
+        ),
+        centerTitle: true,
+      ),
+      body: const FormTask(),
+    );
+  }
+}
