9379135511dc62d0f0d0a03afa03cece5a8b7ea3
karen-oshin <oshindeveloper16@gmail.com>
2021-11-16 10:54:50 -0400

    fixing task helper close #24

--- lib/helper/task_helper.dart
+++ lib/helper/task_helper.dart
@@ -5,12 +5,12 @@ enum Type { work, study, family }
 extension SelectedColor on Task {
   int get selectedColor {
     if (color! > 1 && color! < 20) {
-      return 1;
-    } else if (this.color! > 20 && this.color! < 40) {
       return 0xff997070;
-    } else if (this.color! > 40 && this.color! < 60) {
+    } else if (color! > 20 && color! < 40) {
+      return 0xff997070;
+    } else if (color! > 40 && color! < 60) {
       return 0xff663399;
-    } else if (this.color! > 60 && this.color! < 80) {
+    } else if (color! > 60 && color! < 80) {
       return 0xff008000;
     } else {
       return 0xff008080;
@@ -20,11 +20,11 @@ extension SelectedColor on Task {
 
 extension SelectedType on Task {
   String get selectedType {
-    if (this.type! > 1 && this.type! < 35) {
+    if (type! > 1 && type! < 35) {
       return Type.family.toString().split('.')[1];
-    } else if (this.type! > 35 && this.type! < 70) {
+    } else if (type! > 35 && type! < 70) {
       return Type.work.toString().split('.')[1];
-    } else if (this.type! > 70 && this.type! < 100) {
+    } else if (type! > 70 && type! < 100) {
       return Type.study.toString().split('.')[1];
     } else {
       return Type.work.toString().split('.')[1];
