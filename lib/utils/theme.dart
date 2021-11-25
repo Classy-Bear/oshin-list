0ba8f697d6760e13fce06f79bfe029f853333051
oshindev16 <20186198@itla.edu.do>
2021-11-25 09:52:47 -0400

    Refactor(color) creando e implementando archivo color

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,7 +1,3 @@
 import 'package:flutter/material.dart';
 
 ThemeData mainTheme = ThemeData(primarySwatch: Colors.blue);
-const pastelPink = 0xffF5B7B1;
-const lightBrown = 0xff997070;
-const purple = 0xff663399;
-const green = 0xff008000;
