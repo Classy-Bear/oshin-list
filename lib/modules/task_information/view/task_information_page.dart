d71d12fd2dd0a1a8f96d7c15db931c12fb4c7c3d
oshindev16 <20186198@itla.edu.do>
2021-11-25 11:15:03 -0400

    Refactor(Form) Subiendo logica de formwidget a task information page

--- lib/modules/task_information/view/task_information_page.dart
+++ lib/modules/task_information/view/task_information_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oshin_list/core/widgets/form_task/form.dart';
+import 'package:oshin_list/modules/task_information/bloc/task_form_bloc.dart';
 import 'package:task_repository/task_repository.dart';
 
-class TaskInformationPage extends StatelessWidget {
+class TaskInformationPage extends StatefulWidget {
   static const route = '/taskInformationPage';
 
   const TaskInformationPage({Key? key, required this.task}) : super(key: key);
@@ -10,10 +12,30 @@ class TaskInformationPage extends StatelessWidget {
   final Task? task;
 
   @override
+  State<TaskInformationPage> createState() => _TaskInformationPageState();
+}
+
+class _TaskInformationPageState extends State<TaskInformationPage> {
+  @override
+  void initState() {
+    super.initState();
+    final task = widget.task;
+    if (task != null) {
+      context.read<TaskFormBloc>().titleChanged(task.title ?? '');
+      context.read<TaskFormBloc>().descriptionChanged(task.description ?? '');
+      context.read<TaskFormBloc>().colorChanged(task.color!);
+      context.read<TaskFormBloc>().typeChanged(task.type ?? TaskType.work);
+      context
+          .read<TaskFormBloc>()
+          .dateTimeChanged(task.date?.toIso8601String() ?? '');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Oshin TaskForm"), centerTitle: true),
-      body: FormTaskWidget(task: task),
+      body: FormTaskWidget(task: widget.task),
     );
   }
 }
