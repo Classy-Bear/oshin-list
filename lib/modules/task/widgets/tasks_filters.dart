bb8daa98eefa74a42af9db900b581e0fc568870a
David Acevedo <davidfco1702@protonmail.com>
2021-12-02 20:43:07 -0400

    refactor(tasks_filters) Usando Filters en vez de String

--- lib/modules/task/widgets/tasks_filters.dart
+++ lib/modules/task/widgets/tasks_filters.dart
@@ -1,38 +1,20 @@
 part of 'widgets.dart';
 
-class TaskFiltersWidget extends StatefulWidget {
+class TaskFiltersWidget extends StatelessWidget {
   const TaskFiltersWidget({Key? key}) : super(key: key);
 
   @override
-  _TaskFiltersWidgetState createState() => _TaskFiltersWidgetState();
-}
-
-class _TaskFiltersWidgetState extends State<TaskFiltersWidget> {
-  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: FilterChips(
-        filtersList: const [
-          'Pending',
-          'Done',
-          'OverDue',
-        ],
-        onFilterSelect: (filters) {
-          /// TODO: Use Filters instead of Strings
-          final bloc = context.read<TaskBloc>();
-          for (var filter in filters) {
-            if (filter == 'Pending') {
-              return bloc.setFilter(Filter.pending);
-            } else if (filter == 'Done') {
-              return bloc.setFilter(Filter.completed);
-            } else if (filter == 'OverDue') {
-              return bloc.setFilter(Filter.overdue);
-            }
-          }
-          return bloc.setFilter(Filter.none);
-        },
-      ),
+    final bloc = context.watch<TaskBloc>();
+    return FilterChips<Filter>(
+      selectedFilter: bloc.state.filter,
+      filtersList: const [
+        Filter.pending,
+        Filter.completed,
+        Filter.overdue,
+        Filter.none,
+      ],
+      onFilterSelect: (filters) => bloc.setFilter(filters.first),
     );
   }
 }
